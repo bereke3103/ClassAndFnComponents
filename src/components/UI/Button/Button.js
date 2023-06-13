deb77def4dcef8e024ff9b1a50def3d01e13f6d7
bereke <bereke3103@mail.ru>
2023-06-13 16:01:02 +0600

    switch +

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -2,7 +2,6 @@ import React from 'react';
 import classes from './Button.module.css';
 const Button = ({ type, children, onClick, disabled }) => {
   const cls = [classes.Button, classes[type]];
-  console.log('classes[type]:', classes[type]);
 
   return (
     <button onClick={onClick} disabled={disabled} className={cls.join(' ')}>
---
+++ src/form/formFramework.js
@@ -0,0 +1,36 @@
+export function createControl(config, validation) {
+    return {
+        ...config,
+        validation,
+        valid: !validation,
+        touched: false,
+        value: ''
+    }
+}
+
+export function validate(value, validation = null) {
+    let isValid = true
+    if (!validation) {
+        return true
+    }
+
+    if (validation.required) {
+        isValid = value.trim() !== "" && isValid
+    }
+
+
+
+    return isValid;
+}
+
+export function validateForm(formControls) {
+    let isFormValid = true;
+
+    for(let control in formControls) {
+        if (formControls.hasOwnProperty(control)) {
+            isFormValid = formControls[control].valid && isFormValid;
+        }
+    }
+
+    return isFormValid;
+}
